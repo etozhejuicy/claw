b0c363656494312075d5d130d1d7e99b94b6c522
serj <zenitovic@gmail.com>
2024-04-14 16:18:27 +0300

    fix colors for buttons

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ import "./components/text";
 import "./components/buttons";
 
 document.querySelector("#app").innerHTML = `
-<section>
-  <div class="container">
-    <div class="row g-5">
-      <div id="colors" class="col-12"></div>
-      <div id="text" class="col-12"></div>
-      <div id="buttons" class="col-12"></div>
+<main class="content">
+  <section>
+    <div class="container">
+      <div class="row g-5">
+        <div id="colors" class="col-12"></div>
+        <div id="text" class="col-12"></div>
+        <div id="buttons" class="col-12"></div>
+      </div>
     </div>
-  </div>
-</section>
+  </section>
+</main>
 `;
