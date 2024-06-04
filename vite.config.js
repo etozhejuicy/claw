2ece0f66033695e369b47c934033c63021d1bead
serj <zenitovic@gmail.com>
2024-06-04 14:51:47 +0300

    build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    refresh: true,
+  build: {
+    rollupOptions: {
+      input: {
+        index: "/index.html",
+      },
+    },
+  },
+  refresh: true,
 });
