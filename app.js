5e7a97fa05c6760d49ee9df170b4c2657d296e50
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-06-13 19:47:16 +0300

    update app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// styles
-import "./src/scss/app.scss";
-
-import "./src/js/modules/jquery.js";
-import "./src/js/modules/code.js";
-
-import "lazysizes";
-import "lazysizes/plugins/parent-fit/ls.parent-fit";
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+// styles
+import "../../src/scss/app.scss";
+
+import "./modules/jquery.js";
+import "./modules/code.js";
+
+import "lazysizes";
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
