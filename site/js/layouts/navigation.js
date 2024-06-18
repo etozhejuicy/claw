094fdacf231cac08e545e58d25d9bd5a6d7a4659
Kind Samurai <57537080+etozhejuicy@users.noreply.github.com>
2024-06-18 11:26:16 +0300

    Update navigation.js

--- site/js/layouts/navigation.js
+++ site/js/layouts/navigation.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", (e) => {
   let urlPath = window.location.pathname,
+    urlPathLast = urlPath.substr(urlPath.lastIndexOf('/') + 1),
     links = document.querySelectorAll("[url]");
 
   for (const link of links) {
