0d533a229ed5df43bdef7e7c17596e1590ce8c7b
serj <zenitovic@gmail.com>
2025-02-02 21:22:34 +0300

    Update start.js

--- site/js/components/start.js
+++ site/js/components/start.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#start").innerHTML = `
+    document.querySelector("#start").innerHTML = `
     <div class="row g-5">
         <div class="col-12">
             <div class="card shadow-none">
@@ -11,60 +11,32 @@ window.addEventListener("DOMContentLoaded", () => {
                             </h5>
                         </div>
                         <div class="col-12">
-                            <p>
-                                Для установки потребуется инициализировать команду NPM:
-                            </p>
-                        </div>
-                        <div class="col-12">
-                            <code class="code" data-clipboard-target="#npm-i" disabled>
-                                <div class="code-content">
-                                    <div class="code-body" id="npm-i">npm i claw-css</div>
-                                </div>
-                            </code>
-                        </div>
-                        <div class="col-12">
-                            <p>
-                                Далее необходимо подключить библиотеку, чтобы вы смогли работать с ней в проекте.
-                            </p>
-                        </div>
-                        <div class="col-12">
-                            <p>
-                                Интегрируйте стили в post-css файл вашего проекта при помощи @import:
-                            </p>
-                        </div>
-                        <div class="col-12">
-                            <code class="code" data-clipboard-target="#npm-i" disabled>
-                                <div class="code-content">
-                                    <div class="code-body" id="npm-i">@import "claw-css.min";</div>
-                                </div>
-                            </code>
-                        </div>
-                        <div class="col-12">
-                            <p>
-                                Если вам не требуется установка из NPM и необходима быстрая интеграция с проектом, то просто вставьте этот код в <code>&lthead&gt&lt/head&gt</code>:
-                            </p>
-                        </div>
-                        <div class="col-12">
-                            <code class="code" data-clipboard-target="#npm-i" disabled>
-                                <div class="code-content">
-                                    <div class="code-body" id="npm-i">&ltlink rel="stylesheet" crossorigin href="https://unpkg.com/browse/claw-css@0.0.1/dist/bundle.min.css" /&gt</div>
-                                </div>
-                            </code>
-                        </div>
-                        <div class="col-12">
-                            <div class="badge badge-warning badge-colored w-100 ln-lg">
-                                Внимание! Установка библиотеки через NPM-команду в данный момент недоступна.
+                            <div class="badge badge-danger badge-colored btn-shadow w-100 ln-lg">
+                                Внимание! Доступна версия alpha 0.0.1. Возможны баги и иные неисправности.
                             </div>
                         </div>
                         <div class="col-12">
-                            <div class="badge badge-blue badge-colored w-100 ln-lg">
-                                NPM-команда установки библиотеки и импорт, что представлен выше может измениться со временем.
-                            </div>
+                            <p>
+                                Вы можете добавить следующий <code class="text-warning">&ltlink&gt</code> на вашей странице в <code class="text-warning">&lthead&gt</code> перед своими стилями:
+                            </p>
                         </div>
-                        <div class="col-12 text-center">
-                            <a href="./components" class="btn btn-primary w-100 w-xs-100 w-sm-100 w-md-auto">
-                                Перейти в компоненты
-                            </a>
+                        <div class="col-12">
+                            <code class="code">
+                                <div class="code-content">
+                                    <div class="code-title">
+                                        Пример использования
+                                    </div>
+                                    <pre class="hljs html code-body"><code>&ltlink rel="stylesheet" type="text/css" href="./styles/claw.min.css" /&gt</code></pre>
+                                </div>
+                            </code>
+                        </div>
+                        <div class="col-12">
+                            <p>
+                                Скачать файл можно по ссылке: 
+                            </p>
+                        </div>
+                        <div class="col-auto">
+                            <a href="./build/claw.min.css" class="btn btn-sm btn-success" target="_blank" download="">claw.min.css</a>
                         </div>
                     </div>
                 </div>
